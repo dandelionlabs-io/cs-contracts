88b2d515c399a1db9a4d8b612f8600238389a533
Leon Acosta @ Dandelion Labs <leon@dandelionlabs.io>
2023-01-28 14:25:01 +0700

    Added verification for deployed contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,9 @@
 import * as dotenv from "dotenv";
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
+import "@nomiclabs/hardhat-etherscan";
+import "hardhat-gas-reporter"
 
 dotenv.config();
 
@@ -14,6 +16,11 @@ const config: HardhatUserConfig = {
       network: 5,
       url: process.env.RPC_URL,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    },
+    mainnet: {
+      network: 1,
+      url: process.env.PROD_RPC_URL,
+      accounts: process.env.PROD_PRIVATE_KEY ? [process.env.PROD_PRIVATE_KEY] : []
     }
   },
   solidity: {
@@ -25,6 +32,18 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  gasReporter: {
+    coinmarketcap: 'b5aa6c8e-e54a-4291-b372-6afac549d9fc',
+    currency: 'USD',
+    enabled: true,
+    token: 'ETH'
+  },
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_APIKEY ?? "",
+      mainnet: process.env.PROD_ETHERSCAN_APIKEY ?? ''
+    }
+  }
 };
 
 export default config;
